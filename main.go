78869a45ab6c0bc4541894984bdd0c923bbb72a3
makeworld <makeworld@protonmail.com>
2022-03-03 11:21:56 -0500

    Turned demo code into a minimal daemon
    - Custom config and repo path
    - Leftover useful functions from demo code
    - No HTTP server yet

--- main.go
+++ main.go
@@ -1,25 +1,22 @@
 package main
 
-// Unmodified example code from
+// Adapted from
 // https://github.com/ipfs/go-ipfs/tree/master/docs/examples/go-ipfs-as-a-library
-//
-// Will be changed to something real later
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 	"sync"
 
 	config "github.com/ipfs/go-ipfs-config"
 	files "github.com/ipfs/go-ipfs-files"
 	icore "github.com/ipfs/interface-go-ipfs-core"
-	icorepath "github.com/ipfs/interface-go-ipfs-core/path"
+
+	// icorepath "github.com/ipfs/interface-go-ipfs-core/path"
 	ma "github.com/multiformats/go-multiaddr"
 
 	"github.com/ipfs/go-ipfs/core"
@@ -29,6 +26,8 @@ import (
 	"github.com/ipfs/go-ipfs/repo/fsrepo"
 	"github.com/libp2p/go-libp2p-core/peer"
 )
+
+const ipfsRepoPath = "agregore-ipfs-repo"
 
 /// ------ Setting up the IPFS Repo
 
@@ -51,41 +50,40 @@ func setupPlugins(externalPluginsPath string) error {
 	return nil
 }
 
-func createTempRepo() (string, error) {
-	repoPath, err := ioutil.TempDir("", "ipfs-shell")
+// setupConfig applies custom settings to an IPFS config
+func setupConfig(cfg *config.Config) {
+	// https://github.com/ipfs/go-ipfs/blob/master/docs/config.md
+	// https://github.com/ipfs/go-ipfs/blob/master/docs/experimental-features.md
+
+	// Enable pubsub for better IPNS
+	cfg.Pubsub.Enabled = config.True
+	// Disable API and gateway to prevent malicious apps from using
+	cfg.Addresses.API = []string{}
+	cfg.Addresses.Gateway = []string{}
+}
+
+func createRepo(path string) error {
+	err := os.Mkdir(path, 0755)
 	if err != nil {
-		return "", fmt.Errorf("failed to get temp dir: %s", err)
+		return fmt.Errorf("failed to create repo dir: %s", err)
 	}
 
 	// Create a config with default options and a 2048 bit key
 	cfg, err := config.Init(ioutil.Discard, 2048)
 	if err != nil {
-		return "", err
+		return err
 	}
 
-	// When creating the repository, you can define custom settings on the repository, such as enabling experimental
-	// features (See experimental-features.md) or customizing the gateway endpoint.
-	// To do such things, you should modify the variable `cfg`. For example:
-	if *flagExp {
-		// https://github.com/ipfs/go-ipfs/blob/master/docs/experimental-features.md#ipfs-filestore
-		cfg.Experimental.FilestoreEnabled = true
-		// https://github.com/ipfs/go-ipfs/blob/master/docs/experimental-features.md#ipfs-urlstore
-		cfg.Experimental.UrlstoreEnabled = true
-		// https://github.com/ipfs/go-ipfs/blob/master/docs/experimental-features.md#ipfs-p2p
-		cfg.Experimental.Libp2pStreamMounting = true
-		// https://github.com/ipfs/go-ipfs/blob/master/docs/experimental-features.md#p2p-http-proxy
-		cfg.Experimental.P2pHttpProxy = true
-		// https://github.com/ipfs/go-ipfs/blob/master/docs/experimental-features.md#strategic-providing
-		cfg.Experimental.StrategicProviding = true
-	}
+	// Custom settings
+	setupConfig(cfg)
 
 	// Create the repo with the config
-	err = fsrepo.Init(repoPath, cfg)
+	err = fsrepo.Init(path, cfg)
 	if err != nil {
-		return "", fmt.Errorf("failed to init ephemeral node: %s", err)
+		return fmt.Errorf("failed to init node: %s", err)
 	}
 
-	return repoPath, nil
+	return nil
 }
 
 /// ------ Spawning the node
@@ -98,13 +96,23 @@ func createNode(ctx context.Context, repoPath string) (icore.CoreAPI, error) {
 		return nil, err
 	}
 
+	// Apply custom config, in case this repo was created with an old config
+	cfg, err := repo.Config()
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve repo config: %w", err)
+	}
+	setupConfig(cfg)
+	err = repo.SetConfig(cfg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to set repo config: %w", err)
+	}
+
 	// Construct the node
 
 	nodeOptions := &core.BuildCfg{
 		Online:  true,
-		Routing: libp2p.DHTOption, // This option sets the node to be a full DHT node (both fetching and storing DHT Records)
-		// Routing: libp2p.DHTClientOption, // This option sets the node to be a client DHT node (only fetching records)
-		Repo: repo,
+		Routing: libp2p.DHTOption, // Full DHT node (store and fetch)
+		Repo:    repo,
 	}
 
 	node, err := core.NewNode(ctx, nodeOptions)
@@ -116,39 +124,27 @@ func createNode(ctx context.Context, repoPath string) (icore.CoreAPI, error) {
 	return coreapi.NewCoreAPI(node)
 }
 
-// Spawns a node on the default repo location, if the repo exists
-func spawnDefault(ctx context.Context) (icore.CoreAPI, error) {
-	defaultPath, err := config.PathRoot()
-	if err != nil {
-		// shouldn't be possible
+// Spawns a node on the default repo location, creating it if it doesn't exist
+func spawnNode(ctx context.Context) (icore.CoreAPI, error) {
+	if err := setupPlugins(ipfsRepoPath); err != nil {
 		return nil, err
 	}
 
-	if err := setupPlugins(defaultPath); err != nil {
-		return nil, err
-
+	// Create repo if needed
+	if _, err := os.Stat(ipfsRepoPath); err != nil {
+		if os.IsNotExist(err) {
+			err := createRepo(ipfsRepoPath)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			// Other error: permissions, etc
+			return nil, err
+		}
 	}
 
-	return createNode(ctx, defaultPath)
+	return createNode(ctx, ipfsRepoPath)
 }
-
-// Spawns a node to be used just for this run (i.e. creates a tmp repo)
-func spawnEphemeral(ctx context.Context) (icore.CoreAPI, error) {
-	if err := setupPlugins(""); err != nil {
-		return nil, err
-	}
-
-	// Create a Temporary Repo
-	repoPath, err := createTempRepo()
-	if err != nil {
-		return nil, fmt.Errorf("failed to create temp repo: %s", err)
-	}
-
-	// Spawning an ephemeral IPFS node
-	return createNode(ctx, repoPath)
-}
-
-//
 
 func connectToPeers(ctx context.Context, ipfs icore.CoreAPI, peers []string) error {
 	var wg sync.WaitGroup
@@ -220,152 +216,20 @@ func getUnixfsNode(path string) (files.Node, error) {
 
 /// -------
 
-var flagExp = flag.Bool("experimental", false, "enable experimental features")
-
 func main() {
-	flag.Parse()
-
-	/// --- Part I: Getting a IPFS node running
-
-	fmt.Println("-- Getting an IPFS node running -- ")
+	// Getting a IPFS node running
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	/*
-		// Spawn a node using the default path (~/.ipfs), assuming that a repo exists there already
-		fmt.Println("Spawning node on default repo")
-		ipfs, err := spawnDefault(ctx)
-		if err != nil {
-			panic(fmt.Errorf("failed to spawnDefault node: %s", err))
-		}
-	*/
-
-	// Spawn a node using a temporary path, creating a temporary repo for the run
-	fmt.Println("Spawning node on a temporary repo")
-	ipfs, err := spawnEphemeral(ctx)
+	ipfs, err := spawnNode(ctx)
 	if err != nil {
-		panic(fmt.Errorf("failed to spawn ephemeral node: %s", err))
+		log.Fatalf("failed to spawn node: %s", err)
 	}
 
-	fmt.Println("IPFS node is running")
+	log.Println("IPFS node is running")
 
-	/// --- Part II: Adding a file and a directory to IPFS
-
-	fmt.Println("\n-- Adding and getting back files & directories --")
-
-	inputBasePath := "example-folder/"
-	inputPathFile := inputBasePath + "ipfs.paper.draft3.pdf"
-	inputPathDirectory := inputBasePath + "test-dir"
-
-	someFile, err := getUnixfsNode(inputPathFile)
-	if err != nil {
-		panic(fmt.Errorf("Could not get File: %s", err))
-	}
-
-	cidFile, err := ipfs.Unixfs().Add(ctx, someFile)
-	if err != nil {
-		panic(fmt.Errorf("Could not add File: %s", err))
-	}
-
-	fmt.Printf("Added file to IPFS with CID %s\n", cidFile.String())
-
-	someDirectory, err := getUnixfsNode(inputPathDirectory)
-	if err != nil {
-		panic(fmt.Errorf("Could not get File: %s", err))
-	}
-
-	cidDirectory, err := ipfs.Unixfs().Add(ctx, someDirectory)
-	if err != nil {
-		panic(fmt.Errorf("Could not add Directory: %s", err))
-	}
-
-	fmt.Printf("Added directory to IPFS with CID %s\n", cidDirectory.String())
-
-	/// --- Part III: Getting the file and directory you added back
-
-	outputBasePath, err := ioutil.TempDir("", "example")
-	if err != nil {
-		panic(fmt.Errorf("could not create output dir (%v)", err))
-	}
-	fmt.Printf("output folder: %s\n", outputBasePath)
-	outputPathFile := outputBasePath + strings.Split(cidFile.String(), "/")[2]
-	outputPathDirectory := outputBasePath + strings.Split(cidDirectory.String(), "/")[2]
-
-	rootNodeFile, err := ipfs.Unixfs().Get(ctx, cidFile)
-	if err != nil {
-		panic(fmt.Errorf("Could not get file with CID: %s", err))
-	}
-
-	err = files.WriteTo(rootNodeFile, outputPathFile)
-	if err != nil {
-		panic(fmt.Errorf("Could not write out the fetched CID: %s", err))
-	}
-
-	fmt.Printf("Got file back from IPFS (IPFS path: %s) and wrote it to %s\n", cidFile.String(), outputPathFile)
-
-	rootNodeDirectory, err := ipfs.Unixfs().Get(ctx, cidDirectory)
-	if err != nil {
-		panic(fmt.Errorf("Could not get file with CID: %s", err))
-	}
-
-	err = files.WriteTo(rootNodeDirectory, outputPathDirectory)
-	if err != nil {
-		panic(fmt.Errorf("Could not write out the fetched CID: %s", err))
-	}
-
-	fmt.Printf("Got directory back from IPFS (IPFS path: %s) and wrote it to %s\n", cidDirectory.String(), outputPathDirectory)
-
-	/// --- Part IV: Getting a file from the IPFS Network
-
-	fmt.Println("\n-- Going to connect to a few nodes in the Network as bootstrappers --")
-
-	bootstrapNodes := []string{
-		// IPFS Bootstrapper nodes.
-		"/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
-		"/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
-		"/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
-		"/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
-
-		// IPFS Cluster Pinning nodes
-		"/ip4/138.201.67.219/tcp/4001/p2p/QmUd6zHcbkbcs7SMxwLs48qZVX3vpcM8errYS7xEczwRMA",
-		"/ip4/138.201.67.219/udp/4001/quic/p2p/QmUd6zHcbkbcs7SMxwLs48qZVX3vpcM8errYS7xEczwRMA",
-		"/ip4/138.201.67.220/tcp/4001/p2p/QmNSYxZAiJHeLdkBg38roksAR9So7Y5eojks1yjEcUtZ7i",
-		"/ip4/138.201.67.220/udp/4001/quic/p2p/QmNSYxZAiJHeLdkBg38roksAR9So7Y5eojks1yjEcUtZ7i",
-		"/ip4/138.201.68.74/tcp/4001/p2p/QmdnXwLrC8p1ueiq2Qya8joNvk3TVVDAut7PrikmZwubtR",
-		"/ip4/138.201.68.74/udp/4001/quic/p2p/QmdnXwLrC8p1ueiq2Qya8joNvk3TVVDAut7PrikmZwubtR",
-		"/ip4/94.130.135.167/tcp/4001/p2p/QmUEMvxS2e7iDrereVYc5SWPauXPyNwxcy9BXZrC1QTcHE",
-		"/ip4/94.130.135.167/udp/4001/quic/p2p/QmUEMvxS2e7iDrereVYc5SWPauXPyNwxcy9BXZrC1QTcHE",
-
-		// You can add more nodes here, for example, another IPFS node you might have running locally, mine was:
-		// "/ip4/127.0.0.1/tcp/4010/p2p/QmZp2fhDLxjYue2RiUvLwT9MWdnbDxam32qYFnGmxZDh5L",
-		// "/ip4/127.0.0.1/udp/4010/quic/p2p/QmZp2fhDLxjYue2RiUvLwT9MWdnbDxam32qYFnGmxZDh5L",
-	}
-
-	go func() {
-		err := connectToPeers(ctx, ipfs, bootstrapNodes)
-		if err != nil {
-			log.Printf("failed connect to peers: %s", err)
-		}
-	}()
-
-	exampleCIDStr := "QmUaoioqU7bxezBQZkUcgcSyokatMY71sxsALxQmRRrHrj"
-
-	fmt.Printf("Fetching a file from the network with CID %s\n", exampleCIDStr)
-	outputPath := outputBasePath + exampleCIDStr
-	testCID := icorepath.New(exampleCIDStr)
-
-	rootNode, err := ipfs.Unixfs().Get(ctx, testCID)
-	if err != nil {
-		panic(fmt.Errorf("Could not get file with CID: %s", err))
-	}
-
-	err = files.WriteTo(rootNode, outputPath)
-	if err != nil {
-		panic(fmt.Errorf("Could not write out the fetched CID: %s", err))
-	}
-
-	fmt.Printf("Wrote the file to %s\n", outputPath)
-
-	fmt.Println("\nAll done! You just finalized your first tutorial on how to use go-ipfs as a library")
+	// Let node just run
+	_ = ipfs
+	select {}
 }
