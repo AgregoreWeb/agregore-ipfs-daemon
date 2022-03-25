fc2146fb84cf2a213d8f0e8e4ad9465af06e92b5
makeworld <makeworld@protonmail.com>
2022-03-25 11:17:41 -0400

    Add DELETE support to CORS

--- main.go
+++ main.go
@@ -89,7 +89,7 @@ func setupConfig(cfg *config.Config) {
 
 	// Custom gateway headers
 	cfg.Gateway.HTTPHeaders = make(map[string][]string)
-	cfg.Gateway.HTTPHeaders["Access-Control-Allow-Methods"] = []string{"GET", "HEAD", "POST", "PUT"}
+	cfg.Gateway.HTTPHeaders["Access-Control-Allow-Methods"] = []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
 	cfg.Gateway.HTTPHeaders["Access-Control-Allow-Headers"] = []string{"X-IPFS-Pin"}
 	cfg.Gateway.HTTPHeaders["Access-Control-Expose-Headers"] = []string{"IPFS-Hash", "X-IPFS-Path", "X-IPNS-Path", "Etag"}
 	cfg.Gateway.HTTPHeaders["Access-Control-Max-Age"] = []string{"86400"}
