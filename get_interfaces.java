33bcaf3b17db2d5d67c35d20f4c12239924f0efa
makeworld <makeworld@protonmail.com>
2022-04-21 13:10:34 -0400

    Add missing Java import

--- get_interfaces.java
+++ get_interfaces.java
@@ -11,6 +11,7 @@ import java.lang.StringBuilder;
 import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
 import java.util.Collections;
+import java.util.List
 import java.util.Locale;
 
 public class Example {
